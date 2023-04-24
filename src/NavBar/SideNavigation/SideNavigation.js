f27877bd8e20268137a1f8794ca2851b72fd3243
SuslovAlexander <suslov.itrum@gmail.com>
2023-04-24 18:20:47 +0300

    by directories

--- src/NavBar/SideNavigation/SideNavigation.js
+++ src/NavBar/SideNavigation/SideNavigation.js
@@ -1,9 +1,9 @@
 import classes from './Navigation.module.css'
 import {useState} from 'react';
-import NavItem from "../NavItem/NavItem";
+import NavItem from "./NavBar/NavItem/NavItem";
 
 
-const Navigation = ({onSetContemt}) => {
+const SideNavigation = ({onSetContemt}) => {
     const [list, setList] = useState([
         {id: 1, text: "Продукты", selected: true, name: "products"},
         {id: 2, text: "Пользователи", selected: false, name: "users"},
@@ -17,13 +17,13 @@ const Navigation = ({onSetContemt}) => {
         {id: 10, text: "Промокоды", selected: false, name: "codes"},
     ]);
     const handleClick = (id) => {
-        let temp = list.map(el => {
+        let selectedElem = list.map(el => {
             el.selected = false;
             return el;
         });
-        let clickedItem = temp.find(el => el.id === id);
+        let clickedItem = selectedElem.find(el => el.id === id);
         clickedItem.selected = !clickedItem.selected;
-        setList(temp);
+        setList(selectedElem);
         onSetContemt(id)
     }
 
@@ -39,4 +39,4 @@ const Navigation = ({onSetContemt}) => {
     )
 }
 
-export default Navigation;
+export default SideNavigation;
